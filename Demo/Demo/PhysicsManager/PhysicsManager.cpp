56eda26054bdfe7122a1459faa2641518b4ba93a
mortenhag <71295433+mortenhag@users.noreply.github.com>
2020-09-15 01:10:56 +0200

    Add files via upload

---
+++ Demo/Demo/PhysicsManager/PhysicsManager.cpp
@@ -0,0 +1,11 @@
+#include "PhysicsManager/PhysicsManager.h"
+
+Demo::PhysicsManager::PhysicsManager()
+{
+	std::cout << "PhysicsManager::ctor()" << std::endl;
+}
+
+Demo::PhysicsManager::~PhysicsManager()
+{
+	std::cout << "PhysicsManager::dtor()" << std::endl;
+}
---
+++ Demo/Demo/PhysicsManager/PhysicsManager.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#ifndef _PHYSICSMANAGER_H_
+#define _PHYSICSMANAGER_H_
+namespace Demo
+{
+	class PhysicsManager
+	{
+	public:
+		PhysicsManager();
+		~PhysicsManager();
+	};
+}
+
+#endif 
+
+
+
