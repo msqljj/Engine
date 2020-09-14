c87074cdbdad2ab74276ea47370bf3c0fdd0bae5
mortenhag <71295433+mortenhag@users.noreply.github.com>
2020-09-15 01:04:09 +0200

    Add files via upload

---
+++ Demo/Demo/FileManager/FileManager.cpp
@@ -0,0 +1,11 @@
+#include "FileManager/FileManager.h"
+
+Demo::FileManager::FileManager()
+{
+	std::cout << "FileManager::ctor()" << std::endl;
+}
+
+Demo::FileManager::~FileManager()
+{
+	std::cout << "FileManager::dtor()" << std::endl;
+}
---
+++ Demo/Demo/FileManager/FileManager.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#ifndef _FILEMANAGER_H_
+#define _FILEMANAGER_H_
+namespace Demo
+{
+	class FileManager
+	{
+	public:
+		FileManager();
+		~FileManager();
+	};
+}
+#endif
+
+
+
