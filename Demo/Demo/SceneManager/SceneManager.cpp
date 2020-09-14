ea3e22c4b876602a3b04486a70647ad688da2cd1
mortenhag <71295433+mortenhag@users.noreply.github.com>
2020-09-15 01:17:22 +0200

    Add files via upload

---
+++ Demo/Demo/SceneManager/SceneManager.cpp
@@ -0,0 +1,11 @@
+#include "SceneManager/SceneManager.h"
+
+Demo::SceneManager::SceneManager()
+{
+	std::cout << "SceneManager::ctor()" << std::endl;
+}
+
+Demo::SceneManager::~SceneManager()
+{
+	std::cout << "SceneManager::ctor()" << std::endl;
+}
---
+++ Demo/Demo/SceneManager/SceneManager.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#ifndef _SCENEMANAGER_H_
+#define _SCENEMANAGER_H_
+namespace Demo
+{
+	class SceneManager
+	{
+	public:
+		SceneManager();
+		~SceneManager();
+	};
+}
+
+#endif
+
+
+
